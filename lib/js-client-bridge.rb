e7a1ea7d4907dbca398650e4dc2fcc86b9974e8c
Rolly Fordham <rolly@luma.co.nz>
2010-09-17 12:00:03 +1200

    Forgot to remove some debugging info

--- lib/js-client-bridge.rb
+++ lib/js-client-bridge.rb
@@ -6,7 +6,7 @@ module JsClientBridge
 end
 
 dir = File.join(Pathname(__FILE__).dirname.expand_path, 'js-client-bridge', 'responses/')
-%w(ok error exception validation).each {|f| puts dir + f;require dir + f }
+%w(ok error exception validation).each {|f| require dir + f }
 
 dir = File.join(Pathname(__FILE__).dirname.expand_path + 'js-client-bridge/')
 require dir + 'responses'
